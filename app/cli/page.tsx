443aee79f9dc39ea671edc4b756e6529807d96f8
Alex Hussey <alex@blueguardian.co>
2023-03-06 21:11:30 +1100

    remove deprecated deps

--- app/cli/page.tsx
+++ app/cli/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Script from 'next/script'
-import { Inter } from '@next/font/google'
+import { Inter } from "next/font/google"
 import styles from '../../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
