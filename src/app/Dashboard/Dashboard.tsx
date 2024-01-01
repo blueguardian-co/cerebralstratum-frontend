7838fe056f3d72c7c8836821d9dc3d162fa3ea7b
Alex Hussey <alex@blueguardian.co>
2024-01-01 23:06:32 +0000

    Modify Dashboard

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
-import { PageSection, Title } from '@patternfly/react-core';
+import { PageSection, Title, TextContent, Text, TextVariants } from '@patternfly/react-core';
 
 const Dashboard: React.FunctionComponent = () => (
   <PageSection>
-    <Title headingLevel="h1" size="lg">Dashboard Page Title!</Title>
+    <Title headingLevel="h1" size="lg">Tracker Dashboard</Title>
+    <TextContent>
+      <Text component={TextVariants.p}>
+        Hello World!
+      </Text>
+      <Text component={TextVariants.p}>
+        This is a new paragraph.
+      </Text>
+    </TextContent>
   </PageSection>
 )
 
